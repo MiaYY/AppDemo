9fa9026ba969609bea5a3ee69ea73283738c24fb
MOON <yueyue.ge@hotmail.com>
2026-01-06 10:46:42 +0800

    MooniOSApp
    第一次提交

---
+++ MooniOSApp/MooniOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MooniOSApp
+//
+//  Created by MOON on 2026/1/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MooniOSApp/MooniOSApp/MooniOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MooniOSAppApp.swift
+//  MooniOSApp
+//
+//  Created by MOON on 2026/1/3.
+//
+
+import SwiftUI
+
+@main
+struct MooniOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
